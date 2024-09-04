3b0b9dfc43037d0d1abd37a9f419fe0c56a3c413
Max A <corp.workMA@gmail.com>
2024-09-04 22:18:55 +0300

    add simple sign in component

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,20 +1,66 @@
-import { router } from "expo-router";
-import { Text, View } from "react-native";
-
+import { useState } from "react";
+import {
+  Input,
+  Icon,
+  Stack,
+  Pressable,
+  Center,
+  NativeBaseProvider,
+  Button,
+} from "native-base";
+import { MaterialIcons } from "@expo/vector-icons";
 import { useSession } from "../core/hooks/useSession";
 
 export default function SignIn() {
   const { signIn } = useSession();
+  const [show, setShow] = useState(false);
+
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text
-        onPress={() => {
-          signIn();
-          router.replace("/");
-        }}
-      >
-        Sign In
-      </Text>
-    </View>
+    <NativeBaseProvider>
+      <Center flex={1} px="3">
+        <Stack space={4} w="100%" alignItems="center">
+          <Input
+            w={{
+              base: "75%",
+              md: "25%",
+            }}
+            InputLeftElement={
+              <Icon
+                as={<MaterialIcons name="person" />}
+                size={5}
+                ml="2"
+                color="muted.400"
+              />
+            }
+            placeholder="Name"
+          />
+          <Input
+            w={{
+              base: "75%",
+              md: "25%",
+            }}
+            type={show ? "text" : "password"}
+            InputRightElement={
+              <Pressable onPress={() => setShow(!show)}>
+                <Icon
+                  as={
+                    <MaterialIcons
+                      name={show ? "visibility" : "visibility-off"}
+                    />
+                  }
+                  size={5}
+                  mr="2"
+                  color="muted.400"
+                />
+              </Pressable>
+            }
+            placeholder="Password"
+          />
+          <Button size="md" variant="ghost" onPress={() => signIn()}>
+            Sign in
+          </Button>
+        </Stack>
+      </Center>
+    </NativeBaseProvider>
   );
 }
